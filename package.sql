05557e7b65870358fcff6a4d3052e13be4ffa4ab
Yelyzaveta Krasnyanskaya <lizahtconex@gmail.com>
2020-05-20 21:41:18 +0300

    create package with procedure and function v.2

--- package.sql
+++ package.sql
@@ -1,16 +1,26 @@
 --Firstly compile this--
-CREATE OR REPLACE package demo_package is
+CREATE OR REPLACE package film_package is
     PROCEDURE RemoveGenre (p_name people.people_name%type,
     p_genre genre.genre_name%type);
     
-    FUNCTION SelectFilm (p_budget films_review.budget%type,
-    p_genre films_review.genre%type) RETURN VARCHAR2;
-END demo_package;
+    TYPE film_row IS RECORD (
+    film_name_p films_review.film%type,
+    film_company_p films_review.company%type
+    );
+
+    TYPE film_list_type IS TABLE OF film_row;
+
+    FUNCTION GetFilmList (
+    film_genre films_review.genre%type,
+    film_budget films_review.budget%type
+    ) RETURN film_list_type
+    pipelined;
+END film_package;
 
 --------------------------------------------------------------
 
 --Then compile this block--
-CREATE OR REPLACE package body demo_package is
+CREATE OR REPLACE package body film_package is
     PROCEDURE RemoveGenre (
     p_name people.people_name%type,
     p_genre genre.genre_name%type
@@ -36,27 +46,32 @@ CREATE OR REPLACE package body demo_package is
         THEN DBMS_OUTPUT.PUT_LINE('People or genre not found!');
     END;
     
-    FUNCTION SelectFilm (
-    p_budget films_review.budget%type,
-    p_genre films_review.genre%type
-    )
-    RETURN VARCHAR2
+    FUNCTION GetFilmList (
+        film_genre films_review.genre%type,
+        film_budget films_review.budget%type
+    ) RETURN film_list_type
+    pipelined
     IS 
-        v_film VARCHAR2(50);
     BEGIN
-        SELECT film INTO v_film
-        FROM films_review
-        WHERE budget > p_budget AND genre = p_genre;
-        RETURN v_film;   
-    EXCEPTION
-        WHEN NO_DATA_FOUND 
-        THEN DBMS_OUTPUT.PUT_LINE('Film with this budget or genre not found!');
-        v_film := 'Function finished';
-        RETURN v_film;
-    END;
-END demo_package;
+        for iter in (
+            SELECT film, company
+            FROM films_review
+            WHERE budget > film_budget AND genre = film_genre
+        )
+        LOOP 
+            pipe row(iter);
+        END LOOP;
+    END;   
+END film_package;
+
+-- TEST Package------
+--select * from table(film_package.GetFilmList('Adventure', 100000));
+--
+--begin 
+--    film_package.RemoveGenre('Bob', 'Horror');
+--end;
 
 -------------------------------------------------------------------
 --If you need you can drop function and procedure outside package--
 DROP PROCEDURE RemoveGenre; 
-DROP FUNCTION SelectFilm;
+DROP FUNCTION GetFilmList;
